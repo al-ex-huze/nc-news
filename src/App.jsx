1dcdf0c0a1501faf4367935a8327f77e0bb2a8d4
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-06-18 17:21:24 +0100

    refactors topics links using searchparams instead of function

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/articles/" element={<Articles />} />
-                <Route path="/articles/:topic" element={<Articles />} />
+                <Route path="/articles/" element={<Articles />} />
                 <Route
                     path="/articles/:article_id"
                     element={<ArticlesSingle />}
