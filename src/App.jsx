3f2ab428663865a20728f023036d0527ad3969c2
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-06-21 17:37:02 +0100

    removes index.css and handles in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import Users from "./components/Users";
 
 import RouteError from "./components/Route-Error";
 
-import "../src/styles/index.css"
 import "../src/styles/App.css";
 import "../src/styles/Header.css";
 import "../src/styles/Nav.css";
