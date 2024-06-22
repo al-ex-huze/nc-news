2cd1e7637ebb000e3c3b3d4dc38823879c0cf9f5
Alex Hughes <al-ex-huze@linux-desktop.Home>
2024-06-22 17:26:03 +0100

    implements random article on home page

--- src/components/home/Home-List.jsx
+++ src/components/home/Home-List.jsx
@@ -1,25 +1,60 @@
 import { useEffect, useState } from "react";
 
-const HomeList = () => {
+import ArticlesListCards from "../articles/Articles-List-Cards";
+import ErrorComponent from "../Error-Component";
+
+import { getArticles } from "../../api";
+
+const HomeList = ({ homeArticles, setHomeArticles }) => {
     const [listError, setListError] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
+    const topicFilter = "";
+    const limit = 1;
+    const pageNumber = 1;
+    const [sortByQuery, setSortByQuery] = useState("");
+    const [sortByIsDesc, setSortByIsDesc] = useState(false);
+
     useEffect(() => {
         setIsLoading(true);
-        setShowSortBy(true);
         getArticles(topicFilter, sortByQuery, sortByIsDesc, limit, pageNumber)
             .then(({ articles }) => {
-                setTotalCount(articles[0].total_count);
-                setArticles(articles);
+                let randomArticle = Math.floor(
+                    Math.random() * articles[0].total_count
+                );
+                return getArticles(
+                    topicFilter,
+                    sortByQuery,
+                    sortByIsDesc,
+                    limit,
+                    randomArticle
+                );
+            })
+            .then(({ articles }) => {
+                setHomeArticles(articles);
                 setIsLoading(false);
             })
             .catch((error) => {
                 setListError(error);
             });
     }, [topicFilter, sortByQuery, sortByIsDesc, limit, pageNumber]);
+
+    if (listError) return <ErrorComponent error={listError} />;
+    if (isLoading) return <p>Loading Articles</p>;
     return (
-        <>HOME LIST</>
-    )
+        <div className="Content__list-cards">
+            <ul>
+                {homeArticles.map((article) => {
+                    return (
+                        <ArticlesListCards
+                            article={article}
+                            key={article.article_id}
+                        />
+                    );
+                })}
+            </ul>
+        </div>
+    );
 };
 
 export default HomeList;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 import HomeList from "./Home-List";
 
 const Home = () => {
-    
-const [homeArticles, setHomeArticles] = useState([]);
+    const [homeArticles, setHomeArticles] = useState([]);
 
-
-    return <div className="Content">Home
-    <HomeList />
-    </div>;
+    return (
+        <div className="Content">
+            Random Article
+            <HomeList
+                homeArticles={homeArticles}
+                setHomeArticles={setHomeArticles}
+            />
+        </div>
+    );
 };
 
 export default Home;
